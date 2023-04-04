2ee5cdb9ee6a77b6c78181139ac5d6dfbcd9cda7
Yuriy Zaytsev <y.zaytsev@netris.ru>
2023-04-04 18:59:35 +0300

    Прописаны настройки сервиса

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,10 +2,25 @@ package config
 
 import "github.com/RacoonMediaServer/rms-packages/pkg/configuration"
 
+// Delivery specifies sending details
+type Delivery struct {
+	Sms struct {
+		Account string
+		Key     string
+	}
+	Smtp struct {
+		Host     string
+		Port     int
+		User     string
+		Password string
+	}
+}
+
 // Configuration represents entire service configuration
 type Configuration struct {
-	Http    configuration.Http
-	Monitor configuration.Monitor
+	Http     configuration.Http
+	Monitor  configuration.Monitor
+	Delivery Delivery
 }
 
 var config Configuration
